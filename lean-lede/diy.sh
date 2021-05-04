5dcb5c02a1e40a4e170f4dbcfa03d86ae19e3ec7
hv54 <67815219+hv54@users.noreply.github.com>
2021-05-04 22:15:07 +0800

    Update diy.sh

--- lean-lede/diy.sh
+++ lean-lede/diy.sh
@@ -13,7 +13,7 @@ cd package/custom
 #git clone --depth=1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 https://github.com/vernesong/OpenClash
 #git clone --depth=1 https://github.com/Lienol/openwrt-package
-git clone --depth=1 https://github.com/fw876/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld
 #git clone --depth=1 https://github.com/kenzok8/openwrt-packages
 #git clone --depth=1 https://github.com/kenzok8/small
 
