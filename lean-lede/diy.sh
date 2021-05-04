4330995a6f83f73cd8a366e588fe92480d605a7b
hv54 <67815219+hv54@users.noreply.github.com>
2021-05-04 21:52:57 +0800

    Update diy.sh

--- lean-lede/diy.sh
+++ lean-lede/diy.sh
@@ -5,26 +5,29 @@
 
 mkdir -p package/custom
 cd package/custom
-#git clone https://github.com/coolsnowwolf/packages
-#git clone https://github.com/kenzok8/small
-#git clone https://github.com/kenzok8/openwrt-packages
-#git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
-#git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-#git clone https://github.com/vernesong/OpenClash
-#git clone https://github.com/tty228/luci-app-serverchan
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome
-#git clone https://github.com/pymumu/luci-app-smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns
-#git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-cd ../../
+#git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial
+#git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+#git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
+#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns
+#git clone --depth=1 -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 https://github.com/vernesong/OpenClash
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
+git clone --depth=1 https://github.com/fw876/helloworld
+#git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+#git clone --depth=1 https://github.com/kenzok8/small
+
+cd -
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # Modify default IP
 
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
-# Add some default settings
+# Modify some default settings
 
 #\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
-curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/hv54/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
