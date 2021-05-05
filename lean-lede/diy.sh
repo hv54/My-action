d756f8db5ed09b525c64891afa9180d0263e4635
hv54 <67815219+hv54@users.noreply.github.com>
2021-05-05 11:43:54 +0800

    Update diy.sh

--- lean-lede/diy.sh
+++ lean-lede/diy.sh
@@ -24,8 +24,8 @@ cd -
 
 # Modify default IP
 
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
 
 # Modify some default settings
 
